2798504c890a22b4f2982ca92baa590bb5ac0f55
Y-Wakuta <prsprivatestock@i.softbank.jp>
2016-10-31 21:28:21 +0900

    #7 プログレスバーを追加

--- AlbaMobileAS/app/src/main/java/com/example/rocko/albamobileas/FlightActivity.java
+++ AlbaMobileAS/app/src/main/java/com/example/rocko/albamobileas/FlightActivity.java
@@ -1,19 +1,26 @@
 package com.example.rocko.albamobileas;
 
 import android.content.pm.ActivityInfo;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class FlightActivity extends AppCompatActivity {
 
     TextView DebugButton;
+    ProgressBar MpuLeft;
+    private int _progressBarStatus = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flight);
+
+        MpuLeft = (ProgressBar)findViewById(R.id.MpuLeft);
 
         DebugButton = (TextView) findViewById(R.id.DebugButton);
         DebugButton.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +29,29 @@ public class FlightActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(_progressBarStatus < 100){
+                    _progressBarStatus++;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            MpuLeft.setProgress(_progressBarStatus);
+                        }
+                    });
+                    try {
+                        Thread.sleep(200);
+                    }catch(Exception exc){
+                    }
+                }
+            }
+        }).start();
+        MpuLeft.setMax(100);
+        for(int i = 0;i < 100;i++)
+            MpuLeft.setProgress(i);
+        MpuLeft.setSecondaryProgress(70);
     }
 
 
