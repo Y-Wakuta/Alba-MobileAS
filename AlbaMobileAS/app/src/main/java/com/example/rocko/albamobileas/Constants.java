f700d6e83e12c7d541da363e4d7396deccbcd57b
Y-Wakuta <prsprivatestock@i.softbank.jp>
2016-12-19 21:45:29 +0900

    追加commit

---
+++ AlbaMobileAS/app/src/main/java/com/example/rocko/albamobileas/Constants.java
@@ -0,0 +1,10 @@
+package com.example.rocko.albamobileas;
+
+/**
+ * Created by rocko on 2016/11/01.
+ */
+
+public class Constants {
+   public double MpuDefault = 180.0;
+   public double MpuMoveDeg = 0.3;
+}
